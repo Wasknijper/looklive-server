c7e1158ad218128fb41433324c8cd29a1d64c789
Wasknijper <mudkippers@hotmail.com>
2016-03-16 12:16:45 +0100

    Replaced left with translate + small js fix
    Left originally left in because the transform translate caused a bug
    with a chrome plugin.
    
    Added a check to the ready function so it only exceutes the callback if
    its a function.

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ var llApp = function () {
 	}
 
     function _ready(fn) {
-        if (document.readyState !== 'loading') {
+        if (document.readyState !== 'loading' && fn instanceof Function) {
             fn();
         } else {
             document.addEventListener('DOMContentLoaded', fn);
@@ -33,6 +33,37 @@ var llApp = function () {
     app.wrapper = '';
  	app.feedData = '';
 
+     app.appearance = function() {
+        var firstProduct = document.querySelector('.product');
+        var firstIndicator = document.querySelector(
+            '.product-indicator[data-uuid="' + firstProduct.getAttribute('data-uuid') + '"]'
+        );
+        var indicators = document.querySelectorAll('.product-indicator');
+
+        firstProduct.classList.add('product-active');
+        firstIndicator.classList.add('product-indicator-active');
+
+        Array.prototype.forEach.call(indicators, function (el) {
+            el.addEventListener('click', function (event) {
+                var id = event.currentTarget.getAttribute('data-uuid');
+
+                document
+                    .querySelector('.product-active')
+                    .classList.remove('product-active');
+
+                document
+                    .querySelector('.product-indicator-active')
+                    .classList.remove('product-indicator-active');
+
+                document
+                    .querySelector('.product[data-uuid="' + id + '"]')
+                    .classList.add('product-active');
+
+                event.currentTarget.classList.add('product-indicator-active');
+            });
+        });
+    };
+
     app.init = function(){
     	//router.init();
 
@@ -45,7 +76,7 @@ var llApp = function () {
 
 	    _ready(function () {
 	        if (/appearance/.test(window.location.href)) {
-	            _appearance();
+	            app.appearance();
 	        }
     	});
 
--- public/js/router.js
+++ public/js/router.js
@@ -1,35 +1,5 @@
 var router = (function(){
 
-    function _appearance() {
-        var firstProduct = document.querySelector('.product');
-        var firstIndicator = document.querySelector(
-            '.product-indicator[data-uuid="' + firstProduct.getAttribute('data-uuid') + '"]'
-        );
-        var indicators = document.querySelectorAll('.product-indicator');
-
-        firstProduct.classList.add('product-active');
-        firstIndicator.classList.add('product-indicator-active');
-
-        Array.prototype.forEach.call(indicators, function (el) {
-            el.addEventListener('click', function (event) {
-                var id = event.currentTarget.getAttribute('data-uuid');
-
-                document
-                    .querySelector('.product-active')
-                    .classList.remove('product-active');
-
-                document
-                    .querySelector('.product-indicator-active')
-                    .classList.remove('product-indicator-active');
-
-                document
-                    .querySelector('.product[data-uuid="' + id + '"]')
-                    .classList.add('product-active');
-
-                event.currentTarget.classList.add('product-indicator-active');
-            });
-        });
-    }
 
 	//make a router
 	var routes = new Rlite();
@@ -41,7 +11,7 @@ var router = (function(){
 	routes.add('appearance/:id', function(route){
 		request.get('http://localhost:3000/api/appearance/' + route.params.id, function(d){
 			llApp.wrapper.innerHTML = d;
-			_appearance();
+			llApp.appearance();
 		});
 	});
 	return routes;
